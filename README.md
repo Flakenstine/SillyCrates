fc73d70b7daab18324d093822fec6c8a0f7d70b6
Marinated <johnnyxcook@gmail.com>
2015-09-12 15:13:12 -0400

    Updated README.md and removed comments from CratesPlugin.java

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ MysteryCrates
 A Bukkit server plugin which gives players crates that are placed and opened for some random loot.
 
 =
+
+
+.. image:: http://hydra-media.cursecdn.com/minecraft.gamepedia.com/thumb/d/db/LockedChest.png/440px-LockedChest.png
+   :height: 100px
+   :width: 200 px
+   :scale: 50 %
--- src/main/java/us/mcmagic/mysterycrates/CratesPlugin.java
+++ src/main/java/us/mcmagic/mysterycrates/CratesPlugin.java
@@ -61,15 +61,6 @@ public final class CratesPlugin extends JavaPlugin {
         return crate;
     }
 
-
-//    private ItemStack registerSkull() {
-//        miniCrate = new ItemStack(Material.SKULL, 1);
-//        SkullMeta meta = (SkullMeta) miniCrate.getItemMeta();
-//        meta.setOwner("enderchest");
-//        miniCrate.setItemMeta(meta);
-//        return miniCrate;
-//    }
-
     public static ItemStack getCrateItem() {
         return crate;
     }
@@ -90,99 +81,4 @@ public final class CratesPlugin extends JavaPlugin {
         String[] loc = s.split(",");
         return new Location(Bukkit.getWorld(loc[0]), Double.parseDouble(loc[1]), Double.parseDouble(loc[2]), Double.parseDouble(loc[3]));
     }
-
-    /*
-     - blaze = Blaze Head
-     - cavespider = Cave Spider Head,
-     - chicken = Chicken Head
-     - cow = Cow Head
-     - enderman = Enderman Head
-     - ghast = Mini Ghast Head
-     - golem = Golem Head
-     - lavaslime = Mini Lava Slime
-     - mooshroom = Mooshroom Head
-     - ocelot = Ocelot Head
-     - pig = Pig Head
-     - pigman = Pigman Head
-     - sheep = Sheep Head
-     - slime = Mini Slime
-     - spider = Spider Head
-     - squid = Squid Head
-     - villager = Villager Head
-     - cactus = Mini Cactus
-     - cake = Mini Cake
-     - chest = Mini Chest
-     - melon = Mini Melon
-     - melon2 = Mini Melon
-     - log = Mini Oak Log
-     - wood = Mini Oak Log
-     - wood2 = Mini Oak Log
-     - wood3 = Mini Oak Log
-     - oaklog = Mini Oak Log
-     - oakwood = Mini Oak Log
-     - pumpkin = Mini Pumpkin
-     - pumpkin2 = Mini Pumpkin
-     - pumpkin3 = Mini Pumpkin
-     - tnt = Mini TNT
-     - tnt2 = Mini TNT
-     - arrowup = Arrow Up
-     - arrowdown = Arrow Down
-     - arrowleft = Arrow Left
-     - arrowright = Arrow Right
-     - questionmark = Question Mark
-     - exclamation = Exclamation Mark
-     - leaves = Mini Leaves
-     - planks = Mini Planks
-     - stone = Mini Stone
-     - mossycobble = Mini Mossy Cobblestone
-     - jukebox = Mini Jukebox
-     - dispenser = Mini Dispenser
-     - stickypiston = Mini Sticky Piston
-     - piston = Mini Piston
-     - cobble = Mini Cobblestone
-     - diamondore = Mini Diamond Ore
-     - redstoneore = Mini Redstone Ore
-     - emeraldore = Mini Emerald Ore
-     - sponge = Mini Sponge
-     - quartz = Mini Quartz
-     - bookstack = Book Stack
-     - bookshelf = Mini Bookshelf
-     - haybale = Mini Haybale
-     - spawner = Mini Monster Spawner
-     - glowstone = Mini Glowstone
-     - podzol = Mini Podzol
-     - dirt = Mini Dirt
-     - diamondblock = Mini Diamond Block
-     - goldblock = Mini Gold Block
-     - ironblock = Mini Iron Block
-     - obsidian = Mini Obsidian
-     - bedrock = Mini Bedrock
-     - sand = Mini Sand
-     - redstonelamp = Mini Redstone Lamp
-     - snowman = Mini Redstone Lamp
-     - pufferfish = Pufferfish Head
-     - clownfish = Clownfish Head
-     - popcorn = Popcorn
-     - cupcake = Cupcake
-     - hamburger = Hamburger
-     - globe = Globe
-     - netherrack = Mini Netherrack
-     - monitor = Monitor
-     - tv = TV
-     - radio = Radio
-     - enderchest = Mini Ender Chest
-     - apple = Apple
-     - coconut = Coconut
-     - masterball = Masterball
-     - greatball = Greatball
-     - pokeball = Pokéball
-     - sugarcane = Mini Sugarcane
-     - rubikscube = Rubikscube
-     - clock = Nightstand Clock
-     - ceilinglights = Ceiling Lights
-     - purpleberry = Purple Berry
-     - lemon = Lemon
-     - lime = Lime
-     - cherry = Cherry
-     */
 }
