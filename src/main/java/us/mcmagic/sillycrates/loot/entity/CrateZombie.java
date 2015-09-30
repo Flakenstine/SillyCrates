390aaaa54a772aa83e5bc5418fb465b746f85222
Marinated <johnnyxcook@gmail.com>
2015-09-30 02:11:48 -0400

    Made custom zombie. Will be used as part of CrateEntityLoot in the near future.

--- src/main/java/us/mcmagic/sillycrates/loot/entity/CrateZombie.java
+++ src/main/java/us/mcmagic/sillycrates/loot/entity/CrateZombie.java
@@ -2,11 +2,19 @@ package us.mcmagic.sillycrates.loot.entity;
 
 import net.minecraft.server.v1_8_R3.*;
 import us.mcmagic.sillycrates.SillyCratesMessage;
+import us.mcmagic.sillycrates.util.NMSUtil;
+
+import java.util.List;
 
 public class CrateZombie extends EntityZombie {
 
     public CrateZombie(World world) {
         super(world);
+        // Clears pathfinder goal/target selectors from this zombie.
+        List goalB = (List) NMSUtil.getPrivateField(PathfinderGoalSelector.class, "b", goalSelector); goalB.clear();
+        List goalC = (List) NMSUtil.getPrivateField(PathfinderGoalSelector.class, "c", goalSelector); goalC.clear();
+        List targetB = (List) NMSUtil.getPrivateField(PathfinderGoalSelector.class, "b",  targetSelector); targetB.clear();
+        List targetC = (List) NMSUtil.getPrivateField(PathfinderGoalSelector.class, "c",  targetSelector); targetC.clear();
     }
 
     @Override
