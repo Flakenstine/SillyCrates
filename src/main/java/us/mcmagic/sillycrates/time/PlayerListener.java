a7ff7f0910260a101fbb66df260dfb6aefc9b861
Marinated <johnnyxcook@gmail.com>
2015-09-30 02:14:02 -0400

    Now using PlayerLoginEvent

--- src/main/java/us/mcmagic/sillycrates/time/PlayerListener.java
+++ src/main/java/us/mcmagic/sillycrates/time/PlayerListener.java
@@ -1,21 +1,31 @@
 package us.mcmagic.sillycrates.time;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import us.mcmagic.sillycrates.CratesPlugin;
 
 public class PlayerListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    public void onLogin(PlayerLoginEvent event) {
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onJoin(final PlayerJoinEvent event) {
         CratesPlugin.getInstance().getTimeTracker().addPlayerAsync(event.getPlayer().getUniqueId());
+        Bukkit.getScheduler().runTaskLater(CratesPlugin.getInstance(), new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.getServer().dispatchCommand(event.getPlayer(), "crate amount");
+            }
+        }, 40L);
     }
 
+
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    public void onQuit(PlayerQuitEvent event) {
+    public void onQuit(final PlayerQuitEvent event) {
         CratesPlugin.getInstance().getTimeTracker().removePlayerAsync(event.getPlayer().getUniqueId());
     }
 }
