bcdd39570fbaa1d7754b03114f5fb20fee2b16da
Marinated <johnnyxcook@gmail.com>
2015-09-30 02:16:42 -0400

    Added utility methods

--- src/main/java/us/mcmagic/sillycrates/util/FileUtil.java
+++ src/main/java/us/mcmagic/sillycrates/util/FileUtil.java
@@ -1,5 +1,6 @@
 package us.mcmagic.sillycrates.util;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -50,16 +51,44 @@ public class FileUtil {
         }
     }
 
-    public static void updateTrackedPlayer(TrackedPlayer p) throws IOException {
+    public static ConfigurationSection getPlayersSection() throws IOException {
         ConfigurationSection section = playerYaml.getConfigurationSection("players");
         if (section == null) {
             playerYaml.createSection("players");
             playerYaml.save(playersFile);
         }
+        return section;
+    }
+    public static void registerTrackedPlayer(TrackedPlayer p, final String name) throws IOException {
+        ConfigurationSection section = getPlayersSection();
+        if (section == null) {
+            return;
+        }
         ConfigurationSection player = section.createSection(p.getUniqueId().toString());
         player.set("minutes", p.getPlayTime());
         player.set("crates", p.getAvailableCrates());
-        player.set("name", Bukkit.getPlayer(p.getUniqueId()).getName());
+        try {
+            player.set("name", Bukkit.getPlayer(p.getUniqueId()).getName());
+        } catch (NullPointerException e) {
+            // Try again next update
+        }
+    }
+
+    public static void updateTrackedPlayer(TrackedPlayer p) throws IOException {
+        ConfigurationSection section = getPlayersSection();
+        ConfigurationSection player = section.getConfigurationSection(p.getUniqueId().toString());
+        if (player == null) {
+            return;
+        }
+        player.set("minutes", p.getPlayTime());
+        player.set("crates", p.getAvailableCrates());
+        if (player.get("name") == null) {
+            try {
+                player.set("name", Bukkit.getPlayer(p.getUniqueId()).getName());
+            } catch (NullPointerException e) {
+                // Try again next update
+            }
+        }
         playerYaml.save(playersFile);
     }
 
--- src/main/java/us/mcmagic/sillycrates/util/RandomUtils.java
+++ src/main/java/us/mcmagic/sillycrates/util/RandomUtils.java
@@ -2,7 +2,11 @@ package us.mcmagic.sillycrates.util;
 
 import java.util.Random;
 
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.util.Vector;
 
 public final class RandomUtils {
@@ -12,7 +16,14 @@ public final class RandomUtils {
 	private RandomUtils() {
 		// No instance allowed
 	}
-	
+
+	public static Location getRandomLocation(Location center, int radius) {
+		int x = center.getBlockX() + betweenInclusive(-radius, radius);
+		int y = center.getBlockZ() + betweenInclusive(-radius, radius);
+		int z = center.getBlockZ() + betweenInclusive(-radius, radius);
+		return new Location(center.getWorld(), x, y, z);
+	}
+
 	public static Vector getRandomVector() {
 		double x, y, z;
 		x = random.nextDouble() * 2 - 1;
@@ -31,6 +42,10 @@ public final class RandomUtils {
 		return new Vector(x, 0, z);
 	}
 
+	public static int betweenInclusive(int min, int max) {
+		return random.nextInt(max-min+1) + min;
+	}
+
 	public static Material getRandomMaterial(Material[] materials) {
 		return materials[random.nextInt(materials.length)];
 	}
@@ -38,5 +53,28 @@ public final class RandomUtils {
 	public static double getRandomAngle() {
 		return random.nextDouble() * 2 * Math.PI;
 	}
-	
+
+	private ItemStack[] generateRandomArmor() {
+		ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
+		LeatherArmorMeta helmetMeta = (LeatherArmorMeta) helmet.getItemMeta();
+		helmetMeta.setColor(randomColor());
+		helmet.setItemMeta(helmetMeta);
+		ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+		LeatherArmorMeta chestMeta = (LeatherArmorMeta) helmet.getItemMeta();
+		chestMeta.setColor(randomColor());
+		chest.setItemMeta(chestMeta);
+		ItemStack pants = new ItemStack(Material.LEATHER_LEGGINGS, 1);
+		LeatherArmorMeta pantsMeta = (LeatherArmorMeta) helmet.getItemMeta();
+		pantsMeta.setColor(randomColor());
+		pants.setItemMeta(pantsMeta);
+		ItemStack boots = new ItemStack(Material.LEATHER_BOOTS, 1);
+		LeatherArmorMeta bootsMeta = (LeatherArmorMeta) helmet.getItemMeta();
+		bootsMeta.setColor(randomColor());
+		boots.setItemMeta(bootsMeta);
+		return new ItemStack[]{helmet, chest, pants, boots};
+	}
+
+	private Color randomColor() {
+		return Color.fromRGB(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+	}
 }
